91cf2b7e65275edee8bf1e571a42b65a08b9a094
gajapathylekha-prog <gajapathylekha@gmail.com>
2026-06-10 13:36:57 +0530

    Add files via upload

---
+++ rhpprogram7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        int a1 = a.length();
+        int b1 = b.length();
+        int grid[][] = new int[a1+1][b1+1];
+        int max = 0;
+        for(int i = 1; i <= a1; i++){
+            for(int j = 1; j <= b1; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    grid[i][j] = grid[i-1][j-1]+1;
+                    if(grid[i][j] > max){
+                    max = grid[i][j];
+                }
+                }
+                else{
+                    grid[i][j] = Math.max(grid[i][j-1],grid[i-1][j]);
+                }
+            }
+        }System.out.println(max);
+        
+    }
+}
